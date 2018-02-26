3723c01715509b09ea8a5a89565e04ec44e19212
Jordan Baxter <baxterjo@oregonstate.edu>
2018-02-26 10:33:09 -0800

    Added variable identifiers to header file functions.

--- dot.h
+++ dot.h
@@ -5,13 +5,13 @@ struct dot* dot_construct(float x, float y, float radius, ofColor color);
 
 void dot_draw(struct dot* dot);
 
-void dot_move_down(struct dot*);
+void dot_move_down(struct dot* dot);
 
-void dot_grow(struct dot*);
+void dot_grow(struct dot* dot);
 
-void dot_move_right(struct dot*);
+void dot_move_right(struct dot* dot);
 
-void dot_move_left(struct dot*);
+void dot_move_left(struct dot* dot);
 
-void dot_move_up(struct dot*);
+void dot_move_up(struct dot* dot);
 
